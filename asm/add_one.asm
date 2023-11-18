be5f6a2f0d69baa20a2c1d20d390e86a0742e884
Simone Badaruddin <115610746+simonebadaruddin@users.noreply.github.com>
2023-11-17 22:45:53 -0800

    Delete asm directory

--- asm/add_one.asm
+++
@@ -1,4 +0,0 @@
-   ADD  r1,r0,r0[7]
-   ADD   r2,r1,r0[1]
-   STORE r2,r0,r0[7]
-   HALT  r0,r0,r0
--- asm/badlab.asm
+++
@@ -1,8 +0,0 @@
-#
-# A program with a misspelled label
-#
-	LOAD  r1,pear
-	STORE r1,r0,r0[511]
-	HALT r0,r0,r0
-pair:   DATA 13
-
